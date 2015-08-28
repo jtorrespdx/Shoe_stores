198edf67d2b60a318c7ed049b27f6ce361b75ecb
jose <jtorres.pdx@gmail.com>
2015-08-28 10:55:46 -0700

    adds set/get function for Store.php and passing tests in StoreTest.php.

--- src/Store.php
+++ src/Store.php
@@ -1,8 +1,35 @@
 <?php
     class Store
     {
-        
+            private $store_location;
+            private $id;
+
+            function  __construct($store_location, $id=null)
+            {
+                $this->store_location = $store_location;
+                $this->id = $id;
+            }
+
+            function getStoreLocation()
+            {
+                return $this->store_location;
+            }
+
+            function setStoreLocation($new_store_location)
+            {
+                $this->store_location = (string) $new_store_location;
+            }
+
+
+            // function getId()
+            // {
+            //     return $this->id;
+            // }
+            //
+            // function save
+
+
+
+
     }
-
-
  ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -12,71 +12,100 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    class ShoeTest extends PHPUnit_Framework_TestCase {
+    class StoreTest extends PHPUnit_Framework_TestCase
+    {
 
-        protected function teardown() {
-            Store::deleteAll();
-            Brand::deleteAll();
+        // protected function teardown()
+        // {
+        //     Store::deleteAll();
+        //     Brand::deleteAll();
+        // }
+
+        function testGetStoreLocation()
+        {
+            //Arrange
+            $store_location = "Lloyd Center";
+            $test_store = new Store($store_location);
+
+            //Act
+            $result = $test_store->getStoreLocation();
+
+            //Assert
+            $this->assertEquals($store_location, $result);
+
         }
 
-        function testGetLocation()
+        function testSetStoreLocation()
         {
+            //Arrange
+            $store_location = "Lloyd Center";
+            $test_store = new Store($store_location);
 
-        }
+            //Act
+            $test_store->setStoreLocation("Pioneer Place");
+            $result = $test_store->getStoreLocation();
 
-        function testSetLocation()
-        {
-
+            //Assert
+            $this->assertEquals("Pioneer Place", $result);
         }
 
         function testGetId()
         {
+            //Arrange
+            $store_location = "Lloyd Center";
+            $id = 1;
+            $test_store = new Store($store_location, $id)
 
+            //Act
+            $result = $test_store->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
         }
-
-        function testSave()
-        {
-
-        }
-
-        function testUpdate()
-        {
-
-        }
-
-        function TestDeleteStore()
-        {
-
-        }
-
-        function testGetAll()
-        {
-
-        }
-
-        function testDeleteAll()
-        {
-
-        }
-
-        function testFind()
-        {
-
-        }
-
-        function testAddBrand()
-        {
-
-        }
-
-        function testGetBrands() /*testGetBrand ? */
-        {
-
-        }
-
-        function testDelete()
-        {
-
-        }
+        //
+        // function testSave()
+        // {
+        //
+        // }
+        //
+        // function testUpdate()
+        // {
+        //
+        // }
+        //
+        // function TestDeleteStore()
+        // {
+        //
+        // }
+        //
+        // function testGetAll()
+        // {
+        //
+        // }
+        //
+        // function testDeleteAll()
+        // {
+        //
+        // }
+        //
+        // function testFind()
+        // {
+        //
+        // }
+        //
+        // function testAddBrand()
+        // {
+        //
+        // }
+        //
+        // function testGetBrands() /*testGetBrand ? */
+        // {
+        //
+        // }
+        //
+        // function testDelete()
+        // {
+        //
+        // }
     }
  ?>
